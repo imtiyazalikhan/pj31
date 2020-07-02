9255e28f5fa529e6ad02850c9995dac4330a3ea7
imtiyazalikhan <65025085+imtiyazalikhan@users.noreply.github.com>
2020-07-02 07:15:15 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,36 +1,79 @@
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Body = Matter.Body;
-const Render = Matter.Render;
-
-var pinko1=[]
-var particles=[]
-var ground;
-var wall=[]
+var Engine = Matter.Engine,
+  World = Matter.World,
+  Events = Matter.Events,
+  Bodies = Matter.Bodies;
+ 
+var particles = [];
+var plinkos = [];
 
 var divisionHeight=300;
-
+var score =0;
 function setup() {
-  createCanvas(800,400);
-  rectMode(CENTER);
-for(var k=0;k<=weidth;k=k+80){
-  Divisions.push(new Divisions(k,height-divisionHeight/2,10,divisionHeight));
-}
+  createCanvas(800, 800);
   engine = Engine.create();
   world = engine.world;
-  pinko1=new Pinkos(200,450,40);
-Engine.run(engine);
+  ground = new Ground(width/2,height,width,20);
+
+
+   for (var k = 0; k <=width; k = k + 80) {
+     divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+   }
+
+
+    for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,75));
+    }
+
+    for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,175));
+    }
+
+     for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,275));
+    }
+
+     for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,375));
+    }
+
+    
+
+    
 }
+ 
+
 
 function draw() {
-  background(255,255,255);  
-  pinko1.desplay();
-  for (var j = 0;j<particles.length;j++){
-    particles[j].display();
-  }
-  for(var k = 0; k < Divisions.length; k++){
-    division[k].display();
-  }
-  drawSprites();
+  background("black");
+  textSize(20)
+ //text("Score : "+score,20,30);
+  Engine.update(engine);
+ 
+  
+   for (var i = 0; i < plinkos.length; i++) {
+     
+     plinkos[i].display();
+     
+   }
+   if(frameCount%60===0){
+     particles.push(new particle(random(width/2-30, width/2+30), 10,10));
+     score++;
+   }
+ 
+  for (var j = 0; j < particles.length; j++) {
+   
+     particles[j].display();
+   }
+   for (var k = 0; k < divisions.length; k++) {
+     
+     divisions[k].display();
+   }
 }
